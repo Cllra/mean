a46d430638f17e74bad0393e3f83dcd8dc3e818a
Andrew Throener <athroener@gmail.com>
2015-05-13 14:27:01 -0500

    First commit for admin console

--- public/modules/users/services/authentication.client.service.js
+++ public/modules/users/services/authentication.client.service.js
@@ -3,8 +3,11 @@
 // Authentication service for user variables
 angular.module('users').factory('Authentication', ['$window', function($window) {
 	var auth = {
-		user: $window.user
+		user: $window.user,
+		isAdmin: function () {
+			return (($window.user.roles.indexOf('admin') !== -1) ?  true :  false);
+		}
 	};
-	
+
 	return auth;
 }]);
